a4a034d77d3b92598a06434114317e21740d9d77
Nick Trocino <nicktrocino@gmail.com>
2021-02-15 15:30:38 -0800

    refactored calculator small_programs

--- small_programs/calculator.rb
+++ small_programs/calculator.rb
@@ -1,22 +1,73 @@
-Kernel.puts('First number?: ')
-num1 = Kernel.gets.chomp
-Kernel.puts('Second number?: ')
-num2 = Kernel.gets.chomp
-Kernel.puts('add/subtract/multiply/divide')
-operator = Kernel.gets.chomp.downcase
-
-if operator == 'add'
-  result = num1.to_f + num2.to_f
-elsif operator == 'subtract'
-  result = num1.to_f - num2.to_f
-elsif operator == 'multiply'
-  result = num1.to_f * num2.to_f
-elsif operator == 'divide' && num2.to_f != 0
-  result = num1.to_f / num2.to_f
-elsif num2.to_f.zero?
-  puts 'You cannot divide by zero. Try again.'
-else
-  puts 'Invalid operation. Try again.'
+def prompt(message)
+  puts("=> #{message}")
 end
 
-puts "#{num1} #{operator} #{num2} equals #{result}"
+def valid_number?(num)
+  if num != nil && num.to_i != 0
+    true
+  else
+    prompt("One or more numbers invalid")
+  end
+end
+
+def operation_to_message(op)
+  case op
+  when 'add' then 'adding'
+  when 'subtract' then 'subtracting'
+  when 'multiply' then 'multiplying'
+  when 'divide' then 'dividing'
+  end
+end
+
+
+operators = ['add','subtract','multiply','divide']
+prompt('Welcome to calculator! Enter your name:')
+name = gets.chomp.capitalize
+prompt("Hi there, #{name}")
+
+
+loop do
+  break if name.empty?
+
+  num1 = nil
+  num2 = nil
+  loop do
+    prompt('First number?(zero not allowed): ')
+    num1 = gets.chomp
+    prompt('Second number?(zero not allowed): ')
+    num2 = gets.chomp
+    break if valid_number?(num1) && valid_number?(num2)
+  end
+
+  operator_prompt = <<-MSG
+    What would you like to do?
+    add
+    subtract
+    multiply
+    divide
+  MSG
+  prompt(operator_prompt)
+
+  operator = ''
+  loop do
+    operator = gets.chomp.downcase
+    break if operators.include?(operator)
+    prompt('Please choose a valid operator')
+  end
+
+  result = case operator
+    when 'add' then num1.to_f + num2.to_f
+    when 'subtract' then num1.to_f - num2.to_f
+    when 'multiply' then num1.to_f * num2.to_f
+    when 'divide' then num1.to_f / num2.to_f
+    when num2.to_f.zero? then prompt('You cannot divide by zero. Try again.')
+    else prompt 'Invalid operation. Try again.'
+    end
+
+  prompt("#{operation_to_message(operator)} the two numbers....")
+  prompt("Your result is #{result}") if result != nil
+
+  prompt('Would you like to go again? [yes/no]')
+  continue = gets.chomp.downcase
+  break unless continue == 'yes'
+end
