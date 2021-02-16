fdfc26a1a25e3bad2cc9cdde1e5caf263018f8df
Nick Trocino <nicktrocino@gmail.com>
2021-02-16 15:29:58 -0800

    rock paper scissors solo w/ rubocop ran and minor update

--- small_programs/rps.rb
+++ small_programs/rps.rb
@@ -2,39 +2,43 @@ possible_choices = ['rock', 'paper', 'scissors']
 $user_points = 0
 $computer_points = 0
 
+def prompt(msg)
+  puts "====> #{msg}"
+end
+
 def user_rock(computer)
   if computer == 'paper'
-    puts "Computer picked #{computer}. Computer wins."
+    prompt "Computer picked #{computer}. Computer wins."
     $computer_points += 1
   elsif computer == 'scissors'
-    puts "Computer picked #{computer}. Player wins."
+    prompt "Computer picked #{computer}. Player wins."
     $user_points += 1
   else
-    puts "Computer picked #{computer}. It's a tie."
+    prompt "Computer picked #{computer}. It's a tie."
   end
 end
 
 def user_paper(computer)
   if computer == 'scissors'
-    puts "Computer picked #{computer}. Computer wins."
+    prompt "Computer picked #{computer}. Computer wins."
     $computer_points += 1
   elsif computer == 'rock'
-    puts "Computer picked #{computer}. Player wins."
+    prompt "Computer picked #{computer}. Player wins."
     $user_points += 1
   else
-    puts "Computer picked #{computer}. It's a tie."
+    prompt "Computer picked #{computer}. It's a tie."
   end
 end
 
 def user_scissors(computer)
   if computer == 'rock'
-    puts "Computer picked #{computer}. Computer wins."
+    prompt "Computer picked #{computer}. Computer wins."
     $computer_points += 1
   elsif computer == 'paper'
-    puts "Computer picked #{computer}. Player wins."
+    prompt "Computer picked #{computer}. Player wins."
     $user_points += 1
   else
-    puts "Computer picked #{computer}. It's a tie."
+    prompt "Computer picked #{computer}. It's a tie."
   end
 end
 
@@ -49,7 +53,7 @@ def pick_winner(user, computer)
 end
 
 # Greeting
-puts "Welcome to Rock Paper Scissors! Would you like to play? yes or no"
+prompt "Welcome to Rock Paper Scissors! Would you like to play? yes or no"
 keep_playing = gets.chomp.chars.first
 
 # Game loop
@@ -59,9 +63,10 @@ loop do
   # Sets user choice
   user_choice = nil
   loop do
-    puts "Rock, paper, or scissors?"
+    prompt "Rock, paper, or scissors?"
     user_choice = gets.chomp.downcase
     break if possible_choices.include?(user_choice)
+    prompt "Chose one of the given answers:"
   end
 
   # Sets random choice for computer
@@ -72,18 +77,19 @@ loop do
 
   # Ask if continue
   loop do
-    puts "Would you like to keep going? yes or no."
+    prompt "Would you like to keep going? yes or no."
     keep_playing = gets.chomp.chars.first
     break if ['y', 'n'].include?(keep_playing)
+    prompt "Chose one of the given answers:"
   end
 end
 
 # Final Score and winner
-puts "Final score. Player: #{$user_points} Computer: #{$computer_points}"
+prompt "Final score. Player: #{$user_points} Computer: #{$computer_points}"
 if $user_points > $computer_points
-  puts "Player wins."
+  prompt "Player wins."
 elsif $user_points < $computer_points
-  puts "Computer wins"
+  prompt "Computer wins"
 else
-  puts "It's a tie."
+  prompt "It's a tie."
 end
