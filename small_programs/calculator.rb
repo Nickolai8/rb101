932d6f5867f0f0eb81163a0aa52000bb0922b25a
Nick Trocino <nicktrocino@gmail.com>
2021-02-16 09:12:16 -0800

    made changes to calculator, added yaml file

--- small_programs/calculator.rb
+++ small_programs/calculator.rb
@@ -1,9 +1,13 @@
+require 'pry'
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+
 def prompt(message)
   puts("=> #{message}")
 end
 
 def valid_number?(num)
-  if num != nil && num.to_i != 0
+  if (num.to_f.to_s == num) || (num.to_i.to_s == num)
     true
   else
     prompt("One or more numbers invalid")
@@ -11,20 +15,28 @@ def valid_number?(num)
 end
 
 def operation_to_message(op)
-  case op
+  operator_ing = case op
   when 'add' then 'adding'
   when 'subtract' then 'subtracting'
   when 'multiply' then 'multiplying'
   when 'divide' then 'dividing'
   end
+  # code here before returning case statement result
+  operator_ing
 end
 
+languages = ['english', 'espanol']
+user_language = ''
+loop do
+  prompt(MESSAGES['lang'])
+  user_language = gets.chomp.downcase
+  break if languages.include?(user_language)
+end
 
-operators = ['add','subtract','multiply','divide']
-prompt('Welcome to calculator! Enter your name:')
+possible_operators = ['add', 'subtract', 'multiply', 'divide']
+prompt(MESSAGES[user_language]['welcome'])
 name = gets.chomp.capitalize
-prompt("Hi there, #{name}")
-
+prompt(MESSAGES[user_language]['name_greeting'])
 
 loop do
   break if name.empty?
@@ -32,13 +44,14 @@ loop do
   num1 = nil
   num2 = nil
   loop do
-    prompt('First number?(zero not allowed): ')
+    prompt(MESSAGES[user_language]['first_num'])
     num1 = gets.chomp
-    prompt('Second number?(zero not allowed): ')
+    prompt(MESSAGES[user_language]['second_num'])
     num2 = gets.chomp
     break if valid_number?(num1) && valid_number?(num2)
   end
 
+  # FIXME dont know how to do multi line in yaml files 
   operator_prompt = <<-MSG
     What would you like to do?
     add
@@ -48,26 +61,37 @@ loop do
   MSG
   prompt(operator_prompt)
 
+  prompt(MESSAGES['operator_selection'])
+  
   operator = ''
   loop do
     operator = gets.chomp.downcase
-    break if operators.include?(operator)
-    prompt('Please choose a valid operator')
+    break if possible_operators.include?(operator)
+    prompt(MESSAGES[user_language]['operator_error'])
   end
 
   result = case operator
     when 'add' then num1.to_f + num2.to_f
     when 'subtract' then num1.to_f - num2.to_f
     when 'multiply' then num1.to_f * num2.to_f
-    when 'divide' then num1.to_f / num2.to_f
-    when num2.to_f.zero? then prompt('You cannot divide by zero. Try again.')
-    else prompt 'Invalid operation. Try again.'
+    when 'divide' then num1.to_f / num2.to_f # if num2 not zero add here
+    when num2.to_f.zero? then prompt(MESSAGES[user_language]['divide_by_zero'])
+    else prompt(MESSAGES[user_language]['invalid_op'])
     end
 
-  prompt("#{operation_to_message(operator)} the two numbers....")
-  prompt("Your result is #{result}") if result != nil
+  prompt(MESSAGES[user_language]['operator_in_action'])
+  prompt(MESSAGES[user_language]['result_is']) if result != nil
 
-  prompt('Would you like to go again? [yes/no]')
+  prompt(MESSAGES[user_language]['go_again'])
   continue = gets.chomp.downcase
   break unless continue == 'yes'
 end
+
+############## PRY TEST
+# binding.pry
+# sample_array = nil
+# sample_array.each do
+#   puts ""
+# end
+########################
+
