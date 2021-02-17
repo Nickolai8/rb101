ce5a78c1fd255e206a0a64a423440c3f590176cc
Nick Trocino <nicktrocino@gmail.com>
2021-02-16 17:37:29 -0800

    simplified picking the winner to a hash

--- small_programs/rpslk.rb
+++ small_programs/rpslk.rb
@@ -1,84 +1,35 @@
 possible_choices = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 abbreviated_choices = ['r', 'p', 's', 'l', 'k']
+user_beats = {
+  'rock' => ['lizard', 'scissors'],
+  'r' => ['lizard', 'scissors'],
+  'paper' => ['rock', 'spock'],
+  'p' => ['rock', 'spock'],
+  'scissors' => ['paper', 'lizard'],
+  's' => ['paper', 'lizard'],
+  'lizard' => ['paper', 'spock'],
+  'l' => ['paper', 'spock'],
+  'spock' => ['rock', 'scissors'],
+  'k' => ['rock', 'scissors']
+}
+
 $user_points = 0
 $computer_points = 0
 
+def pick_winner(user, computer, user_win)
+  if user == computer
+    prompt("tie")
+  elsif user_win.include?(computer)
+    prompt("user wins")
+    $user_points += 1
+  else
+    prompt("computer wins")
+    $computer_points += 1
+  end
+end
+
 def prompt(msg)
   puts "====> #{msg}"
-end
-
-def user_rock(computer)
-  if computer == 'paper' || computer == 'spock'
-    prompt "Computer picked #{computer}. Computer wins."
-    $computer_points += 1
-  elsif computer == 'scissors' || computer == 'lizard'
-    prompt "Computer picked #{computer}. Player wins."
-    $user_points += 1
-  else
-    prompt "Computer picked #{computer}. It's a tie."
-  end
-end
-
-def user_paper(computer)
-  if computer == 'scissors' || computer == 'lizard'
-    prompt "Computer picked #{computer}. Computer wins."
-    $computer_points += 1
-  elsif computer == 'rock' || computer == 'spock'
-    prompt "Computer picked #{computer}. Player wins."
-    $user_points += 1
-  else
-    prompt "Computer picked #{computer}. It's a tie."
-  end
-end
-
-def user_scissors(computer)
-  if computer == 'rock' || computer == 'spock'
-    prompt "Computer picked #{computer}. Computer wins."
-    $computer_points += 1
-  elsif computer == 'paper' || computer == 'lizard'
-    prompt "Computer picked #{computer}. Player wins."
-    $user_points += 1
-  else
-    prompt "Computer picked #{computer}. It's a tie."
-  end
-end
-
-def user_spock(computer)
-  if computer == 'paper' || computer == 'lizard'
-    prompt "Computer picked #{computer}. Computer wins."
-    $computer_points += 1
-  elsif computer == 'rock' || computer == 'scissors'
-    prompt "Computer picked #{computer}. Player wins."
-    $user_points += 1
-  else
-    prompt "Computer picked #{computer}. It's a tie."
-  end
-end
-
-def user_lizard(computer)
-  if computer == 'rock' || computer == 'scissors'
-    prompt "Computer picked #{computer}. Computer wins."
-    $computer_points += 1
-  elsif computer == 'paper' || computer == 'spock'
-    prompt "Computer picked #{computer}. Player wins."
-    $user_points += 1
-  else
-    prompt "Computer picked #{computer}. It's a tie."
-  end
-end
-
-def pick_winner(user, computer)
-  if user == 'rock' || user == 'r'
-    user_rock(computer)
-  elsif user == 'paper' || user == 'p'
-    user_paper(computer)
-  elsif user == 'scissors' || user == 's'
-    user_scissors(computer)
-  elsif user == 'lizard' || user == 'l'
-    user_lizard(computer)
-  elsif user == 'spock' || user == 'k'
-    user_spock(computer)
-  end
 end
 
 def declare_winner(user, computer)
@@ -113,7 +64,7 @@ loop do
     loop do
       prompt "Rock(r), paper(p), scissors(s), lizard(l), or Spock(k)?"
       user_choice = gets.chomp.downcase
-      break if possible_choices.include?(user_choice) ||
+      break if possible_choices.include?(user_choice) || 
                abbreviated_choices.include?(user_choice)
       prompt "Chose one of the given answers:"
     end
@@ -122,7 +73,7 @@ loop do
     computer_choice = possible_choices.sample
 
     # Find out the winner
-    pick_winner(user_choice, computer_choice)
+    pick_winner(user_choice, computer_choice, user_beats[user_choice])
   end
 
   # Declare winner of game, reset points
