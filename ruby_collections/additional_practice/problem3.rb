e81c47fac30588c9c34288929f6a09fe0cd577cd
Nick Trocino <nicktrocino@gmail.com>
2021-02-18 14:27:01 -0800

    ruby_collections additional_practice problems done 2nd try

--- ruby_collections/additional_practice/problem3.rb
+++ ruby_collections/additional_practice/problem3.rb
@@ -7,9 +7,3 @@ ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
 ages.each do |k,v|
   ages.delete(k) if v >= 100
 end
-
-# method 2
-ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
-ages.each_with_index do |(k,v), index|
-  # nah
-end
