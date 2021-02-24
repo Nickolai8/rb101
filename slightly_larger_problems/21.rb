26c238b5cf69ccf1d977ab4e4034ac4ca8caeaf1
Nick Trocino <nicktrocino@gmail.com>
2021-02-24 12:39:33 -0800

    21 rough draft done

--- slightly_larger_problems/21.rb
+++ slightly_larger_problems/21.rb
@@ -1,5 +1,4 @@
-require 'pry'
-card_values = %w(Jack Queen King Ace Ace Ace Ace Ace Ace Ace Ace Ace Ace Ace Ace Ace)
+card_values = %w(2 3 4 5 6 7 8 9 10 Jack Queen King Ace)
 suits = %w(Hearts Diamonds Spades Clubs)
 main_deck = []
 discard_deck = []
@@ -7,7 +6,7 @@ player_hand = []
 dealer_hand = []
 player_cards_values = []
 dealer_cards_values = []
-
+winner = nil
 player_points = 0
 dealer_points = 0
 
@@ -18,6 +17,7 @@ def initialize_deck(card_values, suits, deck)
       deck << value + " of " + suit
     end
   end
+  deck.sort_by! { |_| rand }
 end
 
 # shows dealer card and user hand
@@ -36,11 +36,10 @@ end
 
 # player wants to hit?
 def player_hit?
-  hit = nil
+  hit = false
   loop do 
     puts "Player turn. Hit, or stay?"
     player_hit = gets.chomp.downcase
-    hit = false if player_hit == 'stay' 
     hit = true if player_hit == 'hit'
     break if player_hit == 'stay' || player_hit == 'hit'
   end
@@ -56,7 +55,7 @@ end
 
 # dealer should hit?
 def dealer_hit?(dealer, player)
-  (dealer < 17) || (player > dealer) 
+  ( (dealer < 17) || (player > dealer) ) && !(player > 21)
 end
 
 # set points
@@ -69,7 +68,7 @@ def points(cards_values)
     else points += elem.to_i
     end
   end
-
+  # adjusts for aces. Might split into a dif method but technically 1 thing
   if points > 21
     loop do
       break if points < 21 || aces.size <= 0
@@ -81,58 +80,81 @@ def points(cards_values)
   points
 end
 
-# def aces_adjust(cards_values, points)
-
-# end
-
-# create deck, sorts deck
-initialize_deck(card_values, suits, main_deck)
-main_deck.sort_by! { |_| rand }
-
-# moves cards from deck into hands
-player_hand << main_deck.shift(2)
-dealer_hand << main_deck.shift(2)
-player_hand.flatten!
-dealer_hand.flatten!
-
-# extracts cards in hands worths
-player_cards_values = player_hand.map { |el| el.split.first }
-dealer_cards_values = dealer_hand.map { |el| el.split.first }
-
-# initialize beginning points
-player_points = points(player_cards_values)
-dealer_points = points(dealer_cards_values)
-
-# show visible cards at start
-visible_cards(player_hand, dealer_hand)
-
-# player turn
-loop do 
-  break unless player_points < 21
-  if player_hit? 
-    player_hit!(player_hand, main_deck, player_cards_values)
-    visible_cards(player_hand, dealer_hand)
-    player_points = points(player_cards_values)
-  else
-    break
-  end
+# find winner
+def who_won?(player, dealer)
+  if player > 21 then 'dealer'
+  elsif dealer > 21 then 'player'
+  elsif player > dealer then 'player'
+  elsif player < dealer then 'dealer'
+  else 'tie' end
 end
 
-# computer turn
+# print winnter
+def display_winner(winner)
+  unless winner == 'tie' then puts "#{winner} Won"
+  else puts "It's a tie" end
+end
+
 loop do
-  break if player_points == 21 && player_hand.size == 2
-  if player_points < 22 && dealer_hit?(dealer_points, player_points) 
-    p "dealer does hit"
-    dealer_hit!(dealer_hand, main_deck, dealer_cards_values)
-    dealer_points = points(dealer_cards_values)
-  else
-    break
-  end
-end
+  # create deck, sorts deck
+  initialize_deck(card_values, suits, main_deck)
+  # main_deck.sort_by! { |_| rand }
 
+  # moves cards from deck into hands
+  player_hand << main_deck.shift(2)
+  dealer_hand << main_deck.shift(2)
+  player_hand.flatten!
+  dealer_hand.flatten!
+
+  # extracts cards in hands worths
+  player_cards_values = player_hand.map { |el| el.split.first }
+  dealer_cards_values = dealer_hand.map { |el| el.split.first }
+
+  # initialize beginning points
+  player_points = points(player_cards_values)
+  dealer_points = points(dealer_cards_values)
+
+  winner = 'tie' if player_points == 21 && dealer_points == 21
+  winner = 'player' if player_points == 21
+  winner = 'dealer' if dealer_points == 21
+  break if winner
+
+  # show visible cards at start
+  visible_cards(player_hand, dealer_hand)
+
+  # player turn
+  loop do 
+    break unless player_points < 21
+    if player_hit? 
+      player_hit!(player_hand, main_deck, player_cards_values)
+      visible_cards(player_hand, dealer_hand)
+      player_points = points(player_cards_values)
+    else
+      break
+    end
+  end
+
+  # computer turn
+  loop do
+    break if player_points == 21 && player_hand.size == 2
+    if player_points < 22 && dealer_hit?(dealer_points, player_points) 
+      p "dealer does hit"
+      dealer_hit!(dealer_hand, main_deck, dealer_cards_values)
+      dealer_points = points(dealer_cards_values)
+    else
+      break
+    end
+  end
+
+  break
+end
 # end game display
 visible_cards(player_hand, dealer_hand)
 puts "Player points: #{player_points}"
 puts "Player cards: #{player_hand}"
 puts "Dealer points: #{dealer_points}"
 puts "Dealer cards: #{dealer_hand}"
+
+winner = who_won?(player_points, dealer_points)
+display_winner(winner)
+
